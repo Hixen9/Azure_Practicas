c764bf1f0a7ff6115dab42d26b980bf44cac5902
Hixen9 <xbrand9000@gmail.com>
2026-02-10 00:47:23 -0600

    Actualizamos para los metodos POST GET PUT

--- DBmodels/urls.py
+++ DBmodels/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import ImagenesViewSet
 
 router = DefaultRouter()
-router.register(r"productos",ImagenesViewSet)
+router.register(r"productos",ImagenesViewSet,basename="productos")
 
 urlpatterns = [
     path("api/",include(router.urls))
